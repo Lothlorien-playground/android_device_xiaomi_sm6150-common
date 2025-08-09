e47f0c9f822868bd0bd55c1f66252689836bc2c5
AdarshGrewal <adarsh@aosip.dev>
2025-08-09 23:40:51 +0800

    sm6150-common: Remove aosp dalvik-heap configs
    Change-Id: I6ec541a81047f13f891612d952e1a98af4f8e944

--- common.mk
+++ common.mk
@@ -6,9 +6,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 
