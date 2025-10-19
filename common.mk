63c3ce480fd61bd38274d5ddd934a19ed9455a80
Cyber Knight <cyberknight755@gmail.com>
2025-10-20 06:47:55 +0800

    sm6150-common: Build DisplayFeatures
    - Add DisplayFeatures to PRODUCT_PACKAGES.
    - Address DisplayFeatures related SEPolicy rules.
    - Define sysfs_displayfeatures as a domain.
    - Allow system_app to read/write vendor_sysfs_graphics
    - Allow system_app to search vendor_sysfs_graphics
    - Setup permission of nodes relevant to DisplayFeatures at boot.
    - Setup HBM and FPS nodes as RRO for DisplayFeatures.
    
    [galadriel1402]: Adapted for sm6150 devices
    
    Co-authored-by: galadriel1402 <galadriel1402@gmail.com>
    Change-Id: I5597785ab53f4187a284a4cc18638d1f82cefe16
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- common.mk
+++ common.mk
@@ -94,6 +94,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
+# DisplayFeatures
+PRODUCT_PACKAGES += \
+    DisplayFeatures \
+    DisplayFeaturesOverlayCommon
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
